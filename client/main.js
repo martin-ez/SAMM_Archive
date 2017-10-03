1ea00898355038dda40a394ef51a984d9f41c7e6
Lady Jeraldynne Pinzon Capera <lj.pinzon12@uniandes.edu.co>
2017-10-03 12:14:57 -0500

    barra

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('root'));
+    render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
 });
